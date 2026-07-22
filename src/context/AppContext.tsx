62ea980f0d89f812e4f43958941f4541367e6838
Zack289 <ozack911@gmail.com>
2026-07-22 17:40:22 +0545

    Add AppContext for user authentication and enhance ESLint configuration

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+import { authService } from "../main";
+import type { AppContextType, User } from "../types";
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUser() {
+    try {
+      const token = localStorage.getItem("Token");
+
+      const { data } = await axios.get(`${authService}/api/auth/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(data.user);
+      setIsAuth(true);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  /* eslint-disable react-hooks/set-state-in-effect */
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{ isAuth, loading, setIsAuth, setLoading, setUser, user }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppData = (): AppContextType => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error("useAppData must be used within AppProvider");
+  }
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AppProvider } from "./context/AppContext.tsx";
 
 export const authService = "http://localhost:5000";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="454679056804-n7430van2pm5jl21kbpr18j9sm10gqbm.apps.googleusercontent.com">
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
 );
