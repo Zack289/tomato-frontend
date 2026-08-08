aef0f1501d6143bfa155679b02d5f00b9ed971d4
Zack289 <ozack911@gmail.com>
2026-08-08 18:58:34 +0545

    Implement Checkout page with address fetching, order creation, and Razorpay payment integration

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,10 +1,156 @@
+import { useEffect, useState } from "react";
+import { useAppData } from "../context/AppContext";
+import axios from "axios";
+import { restaurantService, utilsService } from "../main";
+import { useNavigate } from "react-router-dom";
+import type { IRestaurant } from "../types";
+import toast from "react-hot-toast";
 
-const Checkout = () => {
-  return (
-    <div>
-      checkout
-    </div>
-  )
+interface Address {
+  _id: string;
+  formattedAddress: string;
+  mobile: number;
 }
 
-export default Checkout
+const Checkout = () => {
+  const navigate = useNavigate();
+
+  const { cart, subTotal, quantity } = useAppData();
+
+  const [addresses, setAddresses] = useState<Address[]>([]);
+
+  const [selectedAddressId, setSelectedAddressId] = useState<string | null>(
+    null,
+  );
+
+  const [loadingAddress, setLoadingAddress] = useState(true);
+  const [loadingRazorpay, setLoadingRazorpay] = useState(false);
+  const [loadingStripe, setLoadingStripe] = useState(false);
+  const [creatingOrder, setCreatingOrder] = useState(false);
+
+  useEffect(() => {
+    const fetchAddresses = async () => {
+      if (!cart || cart.length === 0) {
+        setLoadingAddress(false);
+        return;
+      }
+
+      try {
+        const { data } = await axios.get(
+          `${restaurantService}/api/address/all`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          },
+        );
+
+        setAddresses(data || []);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoadingAddress(false);
+      }
+    };
+
+    fetchAddresses();
+  }, [cart]);
+
+  if (!cart || cart.length === 0) {
+    return (
+      <div className="flex min-h-[60vh] items-center justify-center">
+        <p className="text-gray-500 text-lg">Your cart is empty..</p>
+      </div>
+    );
+  }
+
+  const restaurant = cart[0].restaurantId as IRestaurant;
+
+  const deliveryFee = subTotal < 250 ? 49 : 0;
+  const platformFee = 7;
+  const grandTotal = subTotal + deliveryFee + platformFee;
+
+  const createOrder = async (paymentMethod: "razorpay" | "stripe") => {
+    if (!selectedAddressId) return;
+
+    setCreatingOrder(true);
+
+    try {
+      const { data } = await axios.post(
+        `${restaurantService}/api/order/new`,
+        {
+          paymentMethod,
+          addressId: selectedAddressId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      );
+
+      return data;
+    } catch (error) {
+      toast.error("Failed to create order");
+    } finally {
+      setCreatingOrder(false);
+    }
+  };
+
+  const payWithRazorpay = async () => {
+    try {
+      setLoadingRazorpay(true);
+
+      const order = await createOrder("razorpay");
+      if (!order) return;
+
+      const { orderId, amount } = order;
+
+      const { data } = await axios.post(`${utilsService}/api/payment/create`, {
+        orderId,
+      });
+
+      const { razorpayOrderId, key } = data;
+
+      const options = {
+        key,
+        amount: amount * 100,
+        currency: "INR",
+        name: "Tomato", //your business name
+        description: "Food order payment",
+        // image: "https://example.com/your_logo",
+        order_id: razorpayOrderId,
+        handler: async (response: any) => {
+          try {
+            await axios.post(`${utilsService}/api/payment/verify`, {
+              razorpay_order_id: response.razorpay_order_id,
+              razorpay_payment_id: response.razorpay_payment_id,
+              razorpay_signature: response.razorpay_signature,
+              orderId,
+            });
+
+            toast.success("Payment successful 🎉🍾");
+            navigate('/payment-success/'+ response.razorpay_payment_id)
+          } catch (error) {
+            toast.error("Payment verification failed")
+          }
+        },
+        theme: {
+          color: "#E23744",
+        },
+      };
+
+      const razorpay = new (window as any).Razorpay(options);
+
+      razorpay.open();
+    } catch (error) {
+      console.log(error);
+      toast.error("Payment failed, please refresh page")
+    }finally{
+      setLoadingRazorpay(false)
+    }
+  };
+  return <div>checkout</div>;
+};
+
+export default Checkout;
